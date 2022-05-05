7335e048cdbf68d95f1a642e16f20d45a3b2c5b4
LuccaRebelloToledo <luccarebtoledo@gmail.com>
2022-05-05 20:09:26 -0300

    Homework Rodrigo I

--- Beecrowd1936/src/br/edu/univas/Main.java
+++ Beecrowd1936/src/br/edu/univas/Main.java
@@ -16,16 +16,16 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int[] factory = new int[9];
-        factory[0] = 1;
+        int[] factorial = new int[9];
+        factorial[0] = 1;
         for (int i = 1; i <= 8; i++){
-            factory[i] = i * factory[i-1];
+            factorial[i] = i * factorial[i-1];
         }
         int number = scanner.nextInt();
         int x = 0;
         for(int i = 8; i >= 0; i--){
-            x += (number/factory[i]);
-            number = number % factory[i];
+            x += (number/factorial[i]);
+            number = number % factorial[i];
         }
         System.out.println(x);
     }
